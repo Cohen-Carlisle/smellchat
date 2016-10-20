b432d559d33152cdca08fb3aac1bc095bec44a61
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-10-19 21:34:40 -0400

    remove unnecessary assignment

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
 
   before_save do
-    self.author = author.presence&.squish!
+    author.presence&.squish!
     content.squish!
   end
 end
